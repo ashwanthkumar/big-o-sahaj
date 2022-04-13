e71ceaf254c8a775ec259a9dffd714e1adddc668
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2022-04-13 10:20:27 +0530

    Hasher.Underlying -> Hasher.CreateHash since we don't want to deal with race conditions when trying to do concurrent Hash()

--- hasher.go
+++ hasher.go
@@ -6,29 +6,29 @@ import (
 )
 
 type Hasher struct {
-	Underlying hash.Hash
+	CreateHash func() hash.Hash
 }
 
 func (h *Hasher) Hash(input string) ([]byte, error) {
-	h.Underlying.Reset()
-	_, err := h.Underlying.Write([]byte(input))
+	hasher := h.CreateHash()
+	_, err := hasher.Write([]byte(input))
 	if err != nil {
 		return nil, err
 	}
-	hash := h.Underlying.Sum(nil)
+	hash := hasher.Sum(nil)
 	return hash, nil
 }
 
 func (h *Hasher) Size() int {
-	return h.Underlying.Size()
+	return h.CreateHash().Size()
 }
 
 // New Hasher implementation using SHA-512 from crypto std package
 func NewSha512Hasher() Hasher {
-	return NewCustomHasher(sha512.New())
+	return NewCustomHasher(func() hash.Hash { return sha512.New() })
 }
 
 // New Hasher using any hash.Hash implementation
-func NewCustomHasher(underlying hash.Hash) Hasher {
-	return Hasher{Underlying: underlying}
+func NewCustomHasher(underlying func() hash.Hash) Hasher {
+	return Hasher{CreateHash: underlying}
 }
